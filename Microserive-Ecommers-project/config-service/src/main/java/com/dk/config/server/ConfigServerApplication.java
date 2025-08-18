7be3120829cc5655e4f821ecf1e942190005a1d6
dheeraj khakre <khakredheeraj@gmail.com>
2025-08-18 14:38:49 +0530

    implemented config server within user-service and email service other is remaining

--- Microserive-Ecommers-project/config-service/src/main/java/com/dk/config/server/ConfigServerApplication.java
+++ Microserive-Ecommers-project/config-service/src/main/java/com/dk/config/server/ConfigServerApplication.java
@@ -1,15 +1,15 @@
-package com.ecommerce.emailservice;
+package com.dk.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class EmailServiceApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailServiceApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
